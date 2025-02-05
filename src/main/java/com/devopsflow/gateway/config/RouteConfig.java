91ce35fa4f390d97d5ba1448268dd819063f23f1
KimJongSoo <prjs3643@gmail.com>
2025-02-05 22:13:05 +0900

    [Feat] route 처리

---
+++ src/main/java/com/devopsflow/gateway/config/RouteConfig.java
@@ -0,0 +1,18 @@
+package com.devopsflow.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("portal_route", r -> r.path("/portal/**").filters(f -> f.rewritePath("/portal/(?<segment>.*)", "/${segment}"))
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+
+}
